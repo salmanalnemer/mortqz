79c10351c6466548f0a6183358ae44ec2666819b
salmanalnemer <s.n1166@hotmail.com>
2026-01-03 21:34:26 +0300

    تعديل المكتبات

--- mortqz/settings.py
+++ mortqz/settings.py
@@ -72,8 +72,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = "mortqz.urls"
 WSGI_APPLICATION = "mortqz.wsgi.application"
 
